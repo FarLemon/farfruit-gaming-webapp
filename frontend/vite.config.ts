867748313ade6f46f743d724d72ecfeb010ef59e
Far <FarLemon.FFC@gmail.com>
2023-10-22 08:53:09 -0400

    SolidJS functioning on Firebase

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,22 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-// import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
+  plugins: [solidPlugin()],
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@hooks': '/src/hooks',
+      '@pages': '/src/pages',
+      '@styles': '/src/styles'
+    },
   },
 });
