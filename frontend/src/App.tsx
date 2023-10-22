42644e89ca5f9f1d8324776ba4a388331ecb4ebd
Far <FarLemon.FFC@gmail.com>
2023-10-22 14:06:08 -0400

    reset of structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,13 @@
-import './App.scss';
+import { Component } from 'solid-js';
 
-import { Component, createSignal } from 'solid-js';
-import Counter from './Counter';
+import '@styles/_global.scss'
+import '@/App.scss';
+
+
 
 const App: Component = () => {
-  const [counter, setCounter] = createSignal(0);
-  setInterval(setCounter, 1000, (c: number) => c + 1);
-
   return (
-    <>
-      <div>
-        <h1 class="header">{counter()}</h1>
-      </div>
-      <Counter />
-    </>
+    <div class="page">temp</div>
   );
 };
-
 export default App;
